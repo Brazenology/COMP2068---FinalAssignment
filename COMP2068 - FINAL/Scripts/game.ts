021bf10a44e8707fbde63e5200ee4389f7df0f58
Brazen Kallio <brazen101@hotmail.ca>
2015-04-05 19:35:47 -0400

    Added image pieces (5-10).

--- COMP2068 - FINAL/Scripts/game.ts
+++ COMP2068 - FINAL/Scripts/game.ts
@@ -1,1 +1,36 @@
-﻿
+﻿var canvas;
+var stage: createjs.Stage;
+
+// Game Objects
+var background: createjs.Bitmap;
+var game: createjs.Container; 
+
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    stage.enableMouseOver(20); 
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    stage.update(); // stage refresh
+}
+
+function main() {
+    // instantiate game container
+    game = new createjs.Container();
+
+
+    // Create Dice Roller User Interface
+    createUI();
+    stage.addChild(game);
+}
+
+function createUI(): void {
+    // instantiate my background
+    background = new createjs.Bitmap("assets/images/MonsterMatcherBackground.png");
+    game.addChild(background);
+}
